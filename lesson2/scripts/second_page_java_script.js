1d698baf7d52199876dfbdbc2e671825dd81bfc1
CamilaApai <Camilaapai@hotmail.com>
2022-09-24 12:50:27 -0300

    commit

---
+++ lesson2/scripts/second_page_java_script.js
@@ -0,0 +1,27 @@
+//Get current date
+let date = new Date();
+//console.log(date)
+
+//Get current year
+let currentyear = date.getFullYear();
+//console.log(year)
+
+//Use querySelector to access the HTML element.
+document.querySelector("#currentyear").textContent = currentyear;
+
+//Tranforms lastModified into a Date object.
+let oLastModif = new Date(document.lastModified);
+
+dateformat = new Intl.DateTimeFormat("de-AT", {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+})
+  .format(oLastModif)
+  .replace(/\./g, "/");
+
+//Use querySelector to access the HTML element.
+document.querySelector("#lastModified").textContent = dateformat;
