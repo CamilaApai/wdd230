60f77b2a579c046bbd614add207aab3f42844825
CamilaApai <Camilaapai@hotmail.com>
2022-10-08 22:43:22 -0300

    commit

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -11,17 +11,6 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 
 dateUK.innerHTML = `<em>${fulldateUK}</em>`;
 
-//GETS TOGGLE MENU| Header
-
-function toggleMenu() {
-  document.getElementById("navigation").classList.toggle("open");
-  document.getElementById("hamburgerBtn").classList.toggle("open");
-}
-
-const x = document.getElementById("hamburgerBtn");
-x.onclick = toggleMenu;
-
-
 //GETS CURRENT YEAR | Footer
 //Get current year
 const currentyear = current_date.getFullYear();
