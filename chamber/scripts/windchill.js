191c3a7b20a44de8adce1dd35877dbeb0b798be2
CamilaApai <Camilaapai@hotmail.com>
2022-10-24 07:14:26 -0300

    commit

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,16 +1,19 @@
 //WIND CHILD CALCULATOR| Main - Weather Section
 
-const temperature = document.querySelector("#temperature");
-const windSpeed = document.querySelector("#windspeed");
+const temperature = parseFloat(document.querySelector("#temperature").innerText);
+const windSpeed = parseFloat(document.querySelector("#windspeed").innerText);
 let windChill = "";
 
-if (temperature <= 50  & windSpeed > 3) {
-    windChill = Math.round(35.74 + (0.6215 * temperature) - 
-    (35.75 * (windSpeed ** 0.16)) + (0.4275 ** temperature * (windSpeed ** 0.16)));
+if (temperature <= 50  && windSpeed > 3) {
+    windChill = 35.74 + (0.6215 * temperature) - 
+    (35.75 * (windSpeed ** 0.16)) + (0.4275 * temperature * (windSpeed ** 0.16)); 
+    windChill = Math.round(windChill);
 }
 
 else {
-    windChill = "N/A"
+    windChill = "N/A";
 }
-
 document.querySelector("#windchill").innerHTML = windChill;
+
+
+
