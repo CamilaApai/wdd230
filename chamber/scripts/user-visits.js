fb1d296034f90b685e68ac06e10dc35a075ab6f6
CamilaApai <Camilaapai@hotmail.com>
2022-10-31 00:41:42 -0300

    commit

--- chamber/scripts/user-visits.js
+++ chamber/scripts/user-visits.js
@@ -1,9 +1,10 @@
 // CALCULATES USER NUMBER OF VISITS | Main 
 // initialize display elements
+const todayDisplay = document.querySelector("#lastvisit");
 const visitsDisplay = document.querySelector("#visits");
 
 // get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let numVisits = Number(window.localStorage.getItem("lastvisit"));
 
 // determine if this is the first visit or display the number of visits.
 if (numVisits !== 0) {
@@ -15,6 +16,8 @@ if (numVisits !== 0) {
 // increment the number of visits.
 numVisits++;
 // store the new number of visits value
-localStorage.setItem("visits-ls", numVisits);
+localStorage.setItem("lastvisit", numVisits);
+// show todays date.
+todayDisplay.textContent = Date.now();
 
-// AMOUNT OF DAYS SINCE LAST VISIT | Main 
+
