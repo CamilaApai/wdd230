0faa47fc596a66f853700ee40c2d9b2202054806
CamilaApai <Camilaapai@hotmail.com>
2022-11-03 14:22:21 -0300

    commit

--- lesson7/scripts/base.js
+++ lesson7/scripts/base.js
@@ -1,33 +1,37 @@
-//progressive loading of images
-const img = document.querySelectorAll("[data-src]");
+//Loads all the images with data-src attribute
+const images = document.querySelectorAll("img[data-src]");
 
-function preloadImage(img){
-  const src = img.getAttribute("data-src");
-  if(!src){
-    return;
-  }
-  img.setAttribute('src', img.getAttribute('data-src'));
-  img.onload = () => {
-    img.removeAttribute('data-src');
-  };
+function preloadImage(img) {
+   const src = img.getAttribute("data-src");
+    if(!src) {
+       return;
+    }  
+        img.src = src;
+        img.removeAttribute("data-src");
+    
 }
 
 const imgOptions = {
-  threshold: 1,
-  rootMargin: "0px 0px -10px 0px"
+    threshold: 0,
+    //root: document.querySelector('#scrollArea'),
+	rootMargin: "0px 0px 300px 0px"
+	//
 };
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-  entries.forEach(entry =>{
-    if (!entry.isIntersecting){
-      return;
-    }else {
-      preloadImage(entry.target);
-      imgObserver.unobserve(entry.target);
-    }
-  })
+//Intersection Observer: load target images only when the user scrolls down.
+const imgObserver = new IntersectionObserver((entries, 
+    imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
 }, imgOptions);
 
- img.forEach(img => {
-  imgObserver.observe(img);
-})
+images.forEach(image => {
+    imgObserver.observe(image);
+}); 
+
