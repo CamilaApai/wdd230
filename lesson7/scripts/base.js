aed5b58fcb17f1fd4f907b6acd05d3b1b6a3d38b
CamilaApai <Camilaapai@hotmail.com>
2022-11-03 13:42:05 -0300

    commit

--- lesson7/scripts/base.js
+++ lesson7/scripts/base.js
@@ -1,15 +1,15 @@
 //Loads all the images with data-src attribute
 const images = document.querySelectorAll("img[data-src]");
 
-//function preloadImage(img) {
-//    const src = img.getAttribute("data-src");
-//    if(!src) {
-//        return;
-//    } else {
-//        img.src = src;
+function preloadImage(img) {
+   const src = img.getAttribute("data-src");
+    if(!src) {
+       return;
+    }  
+        img.src = src;
 //        img.removeAttribute("data-src");
-//    }
-//}
+    
+}
 
 const imgOptions = {
     //root: document.querySelector('#scrollArea'),
